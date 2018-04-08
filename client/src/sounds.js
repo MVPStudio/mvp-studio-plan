5cbe2d40fb441c2b06d79acbf63ce9da7a40ff64
James Meyers <james.meyers@emberex.com>
2018-04-08 11:22:09 -0700

    Added bite sound. Added fallback m4a files for sounds.

--- client/src/sounds.js
+++ client/src/sounds.js
@@ -1,19 +1,33 @@
 import { Howl } from 'howler';
 
 export const grab = new Howl({
-  src: [require('./assets/sounds/grab.wav')],
+  src: [
+    require('./assets/sounds/grab.wav'),
+    require('./assets/sounds/grab.m4a'),
+  ],
   volume: 0.2,
 });
 
 export const drop = new Howl({
-  src: [require('./assets/sounds/drop.wav')],
+  src: [
+    require('./assets/sounds/drop.wav'),
+    require('./assets/sounds/drop.m4a'),
+  ],
   volume: 0.2,
 });
 
 export const fire = new Howl({
   src: [
-    require('./assets/sounds/fire.m4a'),
     require('./assets/sounds/fire.wav'),
+    require('./assets/sounds/fire.m4a'),
   ],
   volume: 0.2,
 })
+
+export const bite = new Howl({
+  src: [
+    require('./assets/sounds/bite.wav'),
+    require('./assets/sounds/bite.m4a'),
+  ],
+  volume: 1,
+});
