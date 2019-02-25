eeeb83870fa9ab338aeb617404ea995a6c8d21ae
Scott Lively <scott.lively@gmail.com>
2019-02-24 20:36:24 -0800

    lets repurpose this repo

--- projects/adventure-childrens-museum/client/config/index.js
+++
@@ -1,1 +0,0 @@
-module.exports = Object.assign({}, require('./config.default'), require('./config'));
--- projects/adventure-childrens-museum/client/src/Phaser.js
+++
@@ -1,6 +0,0 @@
-import PIXI from 'pixi.js';
-import p2 from 'p2';
-global.PIXI = PIXI;
-global.p2 = p2;
-import Phaser from 'phaser/dist/phaser';
-export default Phaser;
